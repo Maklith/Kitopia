72b41a88cefa13bbfa8b8962f2646a3e2a794bc1
MakesYT <2696703792@qq.com>
2024-11-22 23:37:13 +0800

    *fix

--- build/Build.cs
+++ build/Build.cs
@@ -254,7 +254,7 @@ class Build : NukeBuild
                     .SetOutput(RootDirectory / "Publish_SelfContained")
                     .SetPublishSingleFile(true)
                     .SetRuntime("win-x64")
-                    .SetFramework("net8.0-windows10.0.17763.0")
+                    .SetFramework("net9.0-windows10.0.17763.0")
                     .SetConfiguration("Release")
                     .SetSelfContained(true)
                 );
