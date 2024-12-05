7fd541ca405a3f79b2949903c3911ed222145df6
MakesYT <2696703792@qq.com>
2024-12-05 19:31:13 +0800

    新增 支持插件自动重新下载图标

--- Core/AvaloniaControl/PluginManagerPage/IconCtr.cs
+++ Core/AvaloniaControl/PluginManagerPage/IconCtr.cs
@@ -7,6 +7,8 @@ using Avalonia.Controls;
 using Avalonia.Data.Converters;
 using Avalonia.Markup.Xaml.MarkupExtensions;
 using Avalonia.Media.Imaging;
+using Core.SDKs.Services.Config;
+using Core.SDKs.Services.Plugin;
 using Core.ViewModel.Pages;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -33,7 +35,29 @@ public class IconCtr : IValueConverter
 
             Task.Run(async () =>
             {
-                onlinePluginInfo.Icon = new Bitmap($"{onlinePluginInfo.Path}{Path.DirectorySeparatorChar}avatar.png");
+                if (!File.Exists($"{onlinePluginInfo.Path}{Path.DirectorySeparatorChar}avatar.png"))
+                {
+                    var request = new HttpRequestMessage()
+                    {
+                        RequestUri = new Uri($"{ConfigManger.ApiUrl}/api/plugin/avatar"),
+                        Method = HttpMethod.Get,
+                    };
+                    request.Headers.Add("id", onlinePluginInfo.Id.ToString());
+                    var sendAsync =await PluginManager._httpClient.SendAsync(request);
+                    var stringAsync =await  sendAsync.Content.ReadAsStringAsync();
+                    var deserializeObject = (JObject)JsonConvert.DeserializeObject(stringAsync);
+                    if (deserializeObject["flag"].ToObject<bool>())
+                    {
+                        var bitmap = new Bitmap(new MemoryStream(deserializeObject["data"].ToObject<byte[]>()));
+                        bitmap.Save($"{onlinePluginInfo.Path}{Path.DirectorySeparatorChar}avatar.png");
+                        onlinePluginInfo.Icon = bitmap;
+                    }
+                }
+                else
+                {
+                    onlinePluginInfo.Icon = new Bitmap($"{onlinePluginInfo.Path}{Path.DirectorySeparatorChar}avatar.png");
+
+                }
             });
 
 
