cf781c6d8c2c03fdd725c3bcd7789a734a3ee5e2
MakesYT <2696703792@qq.com>
2025-01-21 16:18:29 +0800

    *更新版本 0.0.2.100

--- build/Build.cs
+++ build/Build.cs
@@ -380,5 +380,5 @@ class Build : NukeBuild
     ///   - JetBrains Rider            https://nuke.build/rider
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
-    public static int Main() => Execute<Build>(x => x.CompileWindowsX64);
+    public static int Main() => Execute<Build>(x => x.Clean);
 }
