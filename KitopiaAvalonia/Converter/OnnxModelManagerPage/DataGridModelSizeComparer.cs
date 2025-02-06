17db0500f13ec1b6149e684592b7e899e85ce6d6
MakesYT <2696703792@qq.com>
2025-02-06 14:16:50 +0800

    新增 模型大小显示

---
+++ KitopiaAvalonia/Converter/OnnxModelManagerPage/DataGridModelSizeComparer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.IO;
+using Core.SDKs.Services.Plugin;
+using PluginCore.Onnx;
+
+namespace KitopiaAvalonia.Converter.OnnxModelManagerPage;
+
+public class DataGridModelSizeComparer : IComparer
+{
+    public static readonly DataGridModelSizeComparer Default = new DataGridModelSizeComparer();
+
+    public int Compare(object? x, object? y)
+    {
+        if (x is OnnxModelInfoWrapper onnxModelInfoWrapper &&y is OnnxModelInfoWrapper onnxModelInfoWrapper2)
+        {
+            var path = PluginManager.GetPluginByPlgStr(onnxModelInfoWrapper.PluginStr).Path;
+            var fileInfo = new FileInfo($"{path}{onnxModelInfoWrapper.Model.ModelPath}");
+            var path2 = PluginManager.GetPluginByPlgStr(onnxModelInfoWrapper2.PluginStr).Path;
+            var fileInfo2 = new FileInfo($"{path2}{onnxModelInfoWrapper2.Model.ModelPath}");
+            if (fileInfo.Length> fileInfo2.Length)
+            {
+                return 1;
+            }
+
+            if (fileInfo.Length < fileInfo2.Length)
+            {
+                return -1;
+            }
+        }
+
+        return 0;
+    }
+}
---
+++ KitopiaAvalonia/Converter/OnnxModelManagerPage/OnnxModelInfoWrapperToModelSizeCtr.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using Avalonia.Data.Converters;
+using Core.SDKs.Services.Plugin;
+using PluginCore.Onnx;
+
+namespace KitopiaAvalonia.Converter.OnnxModelManagerPage;
+
+public class OnnxModelInfoWrapperToModelSizeCtr : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is OnnxModelInfoWrapper onnxModelInfoWrapper)
+        {
+            var path = PluginManager.GetPluginByPlgStr(onnxModelInfoWrapper.PluginStr).Path;
+            var fileInfo = new FileInfo($"{path}{onnxModelInfoWrapper.Model.ModelPath}");
+            if (fileInfo.Exists)
+            {
+                if (fileInfo.Length>1024*1024 * 1024)  
+                {
+                    return $"{Math.Round((double)fileInfo.Length / (1024 * 1024 * 1024), 2)}GB";
+                }else if (fileInfo.Length > 1024 * 1024)
+                {
+                    return $"{Math.Round((double)fileInfo.Length / (1024 * 1024), 2)}MB";
+                }
+                else
+                {
+                    return $"{Math.Round((double)fileInfo.Length / 1024, 2)}KB";
+                }
+                
+            }
+        }
+
+        return "0B";
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return null;
+    }
+}
