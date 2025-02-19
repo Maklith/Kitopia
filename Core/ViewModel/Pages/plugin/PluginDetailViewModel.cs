6df3d83a402b04ddcfabea8e10acf59908d45193
MakesYT <2696703792@qq.com>
2025-02-19 20:38:46 +0800

    重构 PluginDetailViewModel，移除未使用的代码和API调用

--- Core/ViewModel/Pages/plugin/PluginDetailViewModel.cs
+++ Core/ViewModel/Pages/plugin/PluginDetailViewModel.cs
@@ -8,54 +8,10 @@ namespace Core.ViewModel.Pages.plugin;
 
 public partial class PluginDetailViewModel : ObservableObject
 {
-    internal class ApiResponse
-    {
-        public bool flag { get; set; }
-        public OnlinePluginInfo? data { get; set; }
-    }
-    public string PluginStr;
-    [ObservableProperty] private bool _remote = false;
-    [ObservableProperty] private bool _loading = true;
-    private bool avatarLocalFirst = true;
-    [ObservableProperty]
-    public PluginInfoUiHelper? _pluginInfo;
+    public PluginInfoUiHelper? PluginInfo { get; init; }
     public PluginDetailViewModel(PluginInfoUiHelper pluginStr)
     {
         PluginInfo = pluginStr;
-            //Task.Run(GetInfo);
-    }
-
-    private async Task GetInfo()
-    {
-        await GetOnlinePluginInfo();
-        Loading = false; 
-    }
-    private async Task GetOnlinePluginInfo()
-    {
-        var request = new HttpRequestMessage()
-        {
-            RequestUri = new Uri($"{ConfigManger.ApiUrl}/api/plugin/{PluginStr}"),
-            Method = HttpMethod.Get
-        };
-        var async =await PluginManager._httpClient.SendAsync(request);
-        var stringAsync =await async.Content.ReadAsStringAsync();
-        var options = new JsonSerializerOptions
-        {
-            PropertyNameCaseInsensitive = true
-        };
-        var apiResponse = JsonSerializer.Deserialize<ApiResponse>(stringAsync, options);
-        if (apiResponse is { data: not null })
-        {
-            PluginInfo = new PluginInfoUiHelper()
-            {
-                PluginBaseInfo = apiResponse.data.ToPluginBaseInfo(),
-                OnlinePluginInfo = apiResponse.data,
-                IsLocal = false
-            };
-            Remote = true;
-            
-        }
-          
     }
 
    
