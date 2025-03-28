f872af8b4c847ee8fff1180944b0a070427688eb
MakesYT <2696703792@qq.com>
2025-03-28 16:07:11 +0800

    优化日志文件路径配置

--- Core/SDKs/Services/LogManager.cs
+++ Core/SDKs/Services/LogManager.cs
@@ -7,8 +7,8 @@ namespace Core.SDKs.Services;
 public class LogManager
 {
     public static Logger Logger { get; set; }=new LoggerConfiguration()
-        .WriteTo.File($"logs{Path.DirectorySeparatorChar}info.txt", rollingInterval: RollingInterval.Day,restrictedToMinimumLevel: LogEventLevel.Information )
-        .WriteTo.File($"logs{Path.DirectorySeparatorChar}debug.txt", rollingInterval: RollingInterval.Day,restrictedToMinimumLevel: LogEventLevel.Debug )
+        .WriteTo.File($"{AppDomain.CurrentDomain.BaseDirectory}logs{Path.DirectorySeparatorChar}info.txt", rollingInterval: RollingInterval.Day,restrictedToMinimumLevel: LogEventLevel.Information )
+        .WriteTo.File($"{AppDomain.CurrentDomain.BaseDirectory}logs{Path.DirectorySeparatorChar}debug.txt", rollingInterval: RollingInterval.Day,restrictedToMinimumLevel: LogEventLevel.Debug )
         .WriteTo.Console(outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}][{SourceContext}] {Message:lj}{NewLine}{Exception}")
         .CreateLogger();
 }
