7ac302636979395e6cc3c42c85cd095dddd4bcb4
Maklith <2696703792@qq.com>
2026-01-05 17:11:51 +0800

    *更新版本号至 0.1.1.1

--- Core/ViewModel/Main/MainWindowViewModel.cs
+++ Core/ViewModel/Main/MainWindowViewModel.cs
@@ -21,7 +21,7 @@ public partial class MainWindowViewModel : ObservableRecipient
     [ObservableProperty] private object? _content;
 
     [ObservableProperty] private bool _settingPage = false;
-    [ObservableProperty] private string _version = "0.1.1.0";
+    [ObservableProperty] private string _version = "0.1.1.1";
 
     public MainWindowViewModel()
     {
