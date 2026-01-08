2586ba3ba3c645fc126a85d3fe42f4e5cb1dc4ac
Maklith <2696703792@qq.com>
2026-01-08 13:08:15 +0800

    情景 支持连接方向转换

---
+++ KitopiaAvalonia/FlowToDirectionConverter.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Globalization;
+using Avalonia.Controls;
+using Avalonia.Data.Converters;
+using Core.CustomScenario;
+using NodifyM.Avalonia.Controls;
+using NodifyM.Avalonia.ViewModelBase;
+
+namespace KitopiaAvalonia;
+
+public class FlowToDirectionConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is ConnectorItem connectionViewModelBase)
+        {
+            var connectorType = connectionViewModelBase.ConnectorType;
+            switch (connectorType)
+            {
+                case ConnectorType.Input:
+                    return ConnectionDirection.Backward;
+                case ConnectorType.Output:
+                    return ConnectionDirection.Forward;
+                default:
+                    return ConnectionDirection.Backward;
+            }
+            
+        }
+        return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is ConnectionDirection dir)
+        {
+            return dir == ConnectionDirection.Forward ? ConnectorViewModelBase.ConnectorFlow.Output : ConnectorViewModelBase.ConnectorFlow.Input;
+        }
+
+        return value;
+    }
+}
