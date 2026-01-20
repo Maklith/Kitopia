6ff60bdd8202cf864dc74d871f6ba193a876d4a3
maklith <2696703792@qq.com>
2026-01-20 21:49:33 +0800

    *新增ContextMenu的配套程序和MSIX打包程序

---
+++ ContextMenu.Avalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ContextMenu.Avalonia.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ ContextMenu.Avalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace ContextMenu.Avalonia.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
