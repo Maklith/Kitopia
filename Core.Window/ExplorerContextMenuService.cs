15188fe0a1023b0984733da57f7d156e4e08956c
maklith <2696703792@qq.com>
2026-01-29 11:22:13 +0800

    添加右键菜单功能的安装提示对话框

--- Core.Window/ExplorerContextMenuService.cs
+++ Core.Window/ExplorerContextMenuService.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using PluginCore;
 using Windows.Management.Deployment;
 using Core.Services;
+using Core.Services.Interfaces;
+using Core.Utils;
 using Serilog;
 using Serilog.Core;
 
@@ -12,9 +14,33 @@ namespace Core.Window;
 
 public class ExplorerContextMenuService : IExplorerContextMenuService
 {
+    private readonly IContentDialog _contentDialog;
     private ILogger Logger = LogManager.Logger.ForContext<IExplorerContextMenuService>();
+
+    public ExplorerContextMenuService(IContentDialog contentDialog)
+    {
+        _contentDialog = contentDialog;
+    }
+
     public async Task<bool> RegisterAsync()
     {
+        var packageManager = new PackageManager();
+        var packages = packageManager.FindPackagesForUser(string.Empty);
+        if (packages.All(x => x.Id.Name != "Maklith.KitopiaCompanion"))
+        {
+            var dialog = new DialogContent
+            {
+                Title = "提示",
+                Content = "未检测到Kitopia伴侣程序，请安装以使用右键菜单功能。",
+                PrimaryButtonText = "前往安装",
+                PrimaryAction = () =>
+                {
+                    System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo("ms-windows-store://pdp/?productid=9MV77XCQ37FP") { UseShellExecute = true });
+                }
+            };
+            await _contentDialog.ShowDialogAsync(null, dialog);
+            return false;
+        }
         return false;
     }
 
