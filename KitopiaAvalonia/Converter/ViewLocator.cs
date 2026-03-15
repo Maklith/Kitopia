ed6f96616b613e1b40b1049022a2484dce488cf5
Maklith <2696703792@qq.com>
2026-03-15 20:37:45 +0800

    优化 ViewLocator 逻辑，增加页面缓存和资源释放机制

--- KitopiaAvalonia/Converter/ViewLocator.cs
+++ KitopiaAvalonia/Converter/ViewLocator.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Globalization;
+using System.Threading.Tasks;
 using Avalonia.Controls;
 using Avalonia.Data.Converters;
 using Core.Services.Config;
@@ -12,10 +13,38 @@ namespace KitopiaAvalonia.Converter;
 
 public class ViewLocator : IValueConverter
 {
+    private string? _currentPageKey;
+    private UserControl? _currentPage;
+
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        if (value is not string args) return ServiceManager.Services.GetKeyedService<UserControl>("HomePage");
+        var targetKey = value as string ?? "Home";
 
+        if (_currentPage is not null && string.Equals(_currentPageKey, targetKey, StringComparison.Ordinal))
+        {
+            return _currentPage;
+        }
+
+        var nextPage = ResolvePage(targetKey);
+
+        if (_currentPage is not null)
+        {
+            DisposePage(_currentPage);
+        }
+
+        _currentPageKey = targetKey;
+        _currentPage = nextPage;
+
+        return nextPage;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static UserControl? ResolvePage(string args)
+    {
         switch (args)
         {
             case "Home":
@@ -61,8 +90,30 @@ public class ViewLocator : IValueConverter
         }
     }
 
-    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    private static void DisposePage(UserControl page)
     {
-        throw new NotImplementedException();
+        var dataContext = page.DataContext;
+
+        TryDispose(dataContext);
+
+        if (!ReferenceEquals(page, dataContext))
+        {
+            TryDispose(page);
+        }
+    }
+
+    private static void TryDispose(object? instance)
+    {
+        switch (instance)
+        {
+            case null:
+                return;
+            case IAsyncDisposable asyncDisposable:
+                asyncDisposable.DisposeAsync().AsTask().GetAwaiter().GetResult();
+                return;
+            case IDisposable disposable:
+                disposable.Dispose();
+                return;
+        }
     }
 }
