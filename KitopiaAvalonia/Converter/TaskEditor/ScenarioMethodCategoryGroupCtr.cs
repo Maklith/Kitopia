a80c59f4e8663ae3b2335a60b66b21367b1992b8
Maklith <2696703792@qq.com>
2026-05-13 21:03:44 +0800

    修复 情景编辑 节点列表无法正确显示

--- KitopiaAvalonia/Converter/TaskEditor/ScenarioMethodCategoryGroupCtr.cs
+++ KitopiaAvalonia/Converter/TaskEditor/ScenarioMethodCategoryGroupCtr.cs
@@ -4,19 +4,31 @@ using Avalonia.Controls;
 using Avalonia.Controls.Templates;
 using Avalonia.Data.Converters;
 using Core.CustomScenario;
+using KitopiaAvalonia.Windows.TaskEditors;
 
 namespace KitopiaAvalonia.Converter.TaskEditor;
 
 public class ScenarioMethodCategoryGroupCtr : IValueConverter
 {
-    public IDataTemplate DataTemplate;
+    private IDataTemplate? _dataTemplate;
 
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        if (parameter is Control control)
+        if (parameter is IDataTemplate template)
+        {
+            _dataTemplate = template;
+        }
+        else if (parameter is Control control)
         {
             control.TryGetResource("ScenarioMethodNode", null, out var dataTemplate);
-            DataTemplate = dataTemplate as IDataTemplate;
+            _dataTemplate = dataTemplate as IDataTemplate;
+        }
+
+        _dataTemplate ??= new NodeTemplatesSelector { TemplateType = NodeRenderType.View };
+
+        if (value is not ScenarioMethodCategoryGroup group)
+        {
+            return null;
         }
 
         var expander = new Expander();
@@ -24,15 +36,9 @@ public class ScenarioMethodCategoryGroupCtr : IValueConverter
         var itemsControl = new StackPanel();
         itemsControl.Spacing = 5;
 
-        //itemsControl.ItemTemplate=itemsControl.GetR
-        if (value is ScenarioMethodCategoryGroup group)
-        {
-            expander.Header = "节点";
-
-
-            expander.Content = itemsControl;
-            Prase(group, itemsControl);
-        }
+        expander.Header = "节点";
+        expander.Content = itemsControl;
+        Prase(group, itemsControl);
 
         return expander;
     }
@@ -57,9 +63,9 @@ public class ScenarioMethodCategoryGroupCtr : IValueConverter
         }
 
         foreach (var (key, value) in group.Methods)
-            if (DataTemplate.Match(value))
+            if (_dataTemplate!.Match(value))
             {
-                var control = DataTemplate.Build(value);
+                var control = _dataTemplate.Build(value);
                 control.DataContext = value;
                 itemsControl.Children.Add(control);
             }
