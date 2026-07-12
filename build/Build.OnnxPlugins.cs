6103f1f0dca1bc3e61f3e91b5df9e0df60784886
Maklith <2696703792@qq.com>
2026-07-12 21:01:44 +0800

    *更新版本号至 0.2.2.0

---
+++ build/Build.OnnxPlugins.cs
@@ -0,0 +1,39 @@
+using System.IO.Compression;
+using Nuke.Common;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Serilog;
+
+partial class Build
+{
+    Target PackOnnxRuntimeGpuWin => _ => _
+        .DependsOn(CreateRelease, RestoreWindows)
+        .Executes(() => PublishStandaloneOnnxPlugin(
+            RootDirectory / "OnnxRuntime.Gpu.Win" / "OnnxRuntime.Gpu.Win.csproj",
+            "OnnxRuntime.Gpu.Win"));
+
+    Target PackOnnxRuntimeOpenVino => _ => _
+        .DependsOn(CreateRelease, RestoreWindows)
+        .Executes(() => PublishStandaloneOnnxPlugin(
+            RootDirectory / "OnnxRuntime.OpenVino" / "OnnxRuntime.OpenVino.csproj",
+            "OnnxRuntime.OpenVino"));
+
+    Target PackOnnxPlugins => _ => _
+        .DependsOn(PackOnnxRuntimeGpuWin, PackOnnxRuntimeOpenVino);
+
+    void PublishStandaloneOnnxPlugin(AbsolutePath project, string artifactName)
+    {
+        const string runtime = "win-x64";
+        var output = ArtifactsDirectory / "plugins" / artifactName / runtime;
+        output.DeleteDirectory();
+        PublishPlugin(project, runtime, output);
+        RemoveSymbolsAndDocs(output);
+
+        var archive = RootDirectory /
+                      $"Kitopia{AvaloniaProject.GetProperty("Version")}_{artifactName}_{runtime}.zip";
+        archive.DeleteFile();
+        output.ZipTo(archive, compressionLevel: CompressionLevel.SmallestSize);
+        Log.Information("Created standalone ONNX plugin artifact {Artifact}", archive);
+        UploadReleaseAsset(archive);
+    }
+}
